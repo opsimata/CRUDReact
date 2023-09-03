caaff321b67d99b2a93db89844daa9dcdb590be4
Pedro <pedro.enrique1599@icloud.com>
2023-09-03 09:01:15 -0300

    front-end created

---
+++ crud-react-main/frontend/src/App.js
@@ -0,0 +1,33 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Container = styled.div`
+	width: 100%;
+	max-width: 800px;
+	margin-top: 20px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 10px;
+`;
+
+const Title = styled.h2``;
+
+function App() {
+
+	return (
+		<>
+			<Container>
+				<Title>Usuários</Title>
+				<Form />
+			</Container>
+			<ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+			<GlobalStyle />
+		</>
+	);
+}
+
+export default App;
